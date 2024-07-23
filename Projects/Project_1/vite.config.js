f145ef5d2d7057ea96b6467187ed4480e1ad6613
alraven3 <alraven@protonmail.com>
2024-07-23 14:43:25 +0200

    flkgl

--- Projects/Project_1/vite.config.js
+++ Projects/Project_1/vite.config.js
@@ -3,5 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: "/Projects/Project_1/", // change to your repo name
 	plugins: [svelte()],
 });
